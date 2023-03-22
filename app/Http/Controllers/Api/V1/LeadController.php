d610f06d33e914de70458e28ded9d595e6390d9b
Josh Campbell <josh.campbell4000@gmail.com>
2023-03-23 00:11:25 +1030

    refactor LeadController to shift logic to LeadQuery class

--- app/Http/Controllers/Api/V1/LeadController.php
+++ app/Http/Controllers/Api/V1/LeadController.php
@@ -8,13 +8,12 @@ use App\Models\Lead;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\V1\AddressResource;
 use App\Http\Resources\V1\LeadResource;
-use App\Http\Resources\V1\LeadCollection;
-use App\Models\Address;
 use App\Services\V1\LeadQuery;
-use Exception;
 use Illuminate\Http\Request;
-use Throwable;
 
+/**
+ * @package App\Http\Controllers\Api\V1
+ */
 class LeadController extends Controller
 {
     /**
@@ -23,32 +22,7 @@ class LeadController extends Controller
     public function index(Request $request)
     {
         $query = new LeadQuery();
-        $queryItems = $query->transform($request);
-        $leads = Lead::where($queryItems)
-            ->whereNull('deleted_at')
-            ->with('address');
-
-        $bill_threshold = env('ELECTRIC_BILL_THRESHOLD', 250);
-        $quality = $request->query('quality');
-
-        if ($quality) {
-            if ($quality === 'premium') {
-                $leads->where('electric_bill', '>=', $bill_threshold);
-            } elseif ($quality === 'standard') {
-                $leads->where('electric_bill', '<', $bill_threshold);
-            }
-        }
-
-        return new LeadCollection($leads->paginate()->appends($request->query()));
-        
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
+        return $query->getIndexResults($request);        
     }
 
     /**
@@ -56,12 +30,8 @@ class LeadController extends Controller
      */
     public function store(StoreLeadRequest $request)
     {
-        $request_params = $request->all();        
-        $lead = new LeadResource(Lead::create($request->all()));
-        $request_params['lead_id'] = $lead->id;
-        $address = new AddressResource(Address::create($request_params));
-
-        return response()->json(['lead' => $lead, 'address' => $address]);
+        $query = new LeadQuery();
+        return $query->createLeadFromApi($request); 
     }
 
     /**
@@ -73,14 +43,6 @@ class LeadController extends Controller
             'lead' => new LeadResource($lead), 
             'address' => new AddressResource($lead->address)
         ]);
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(UpdateLeadRequest $request, Lead $lead)
-    {
-        
     }
 
     /**
@@ -97,21 +59,7 @@ class LeadController extends Controller
      */
     public function destroy(Lead $lead)
     {
-        $lead_id = $lead->id;
-        $code = 200;
-        $message = '';
-
-        try{
-            $lead->delete();
-            $message = sprintf('Lead with ID: %d successfully deleted', $lead_id);
-        } catch (Throwable $e) {
-            $code = 500;
-            $message = $e->getMessage();
-        }
-
-        return response()->json([
-            'code' => $code,
-            'message' => $message
-        ]);
+        $query = new LeadQuery();
+        return $query->deleteLeadFromApi($lead);
     }
 }
--- app/Services/V1/LeadQuery.php
+++ app/Services/V1/LeadQuery.php
@@ -2,10 +2,32 @@
 
 namespace App\Services\V1;
 
+use App\Http\Resources\V1\AddressResource;
+use App\Http\Resources\V1\LeadCollection;
+use App\Http\Resources\V1\LeadResource;
+use App\Models\Address;
+use App\Models\Lead;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Contracts\Container\BindingResolutionException;
 use Illuminate\Http\Request;
+use InvalidArgumentException;
+use Throwable;
 
+/**
+ * 
+ * @package App\Services\V1
+ */
 class LeadQuery
 {
+    /**
+     * @var int
+     */
+    protected $defaultThreshold = 250;
+
+    /**
+     * 
+     * @var string[][]
+     */
     protected $allowedFields = [
         'id' => ['=', '<', '>'],
         'firstName' => ['=', 'like'],
@@ -15,13 +37,38 @@ class LeadQuery
         'electricBill' => ['=', '<', '>'],
     ];
 
+    /**
+     * 
+     * @var string[]
+     */
     protected $columnMap = [
         'electricBill' => 'electric_bill',
         'firstName' => 'first_name',
         'lastName' => 'last_name',
     ];
 
-    public function transform(Request $request) 
+    /**
+     * @return int 
+     */
+    public function getDefaultThreshold(): int
+    {
+        return $this->defaultThreshold;
+    }
+
+    /**
+     * @param int $threshold 
+     * @return void 
+     */
+    public function setDefaultThreshold(int $threshold): void
+    {
+        $this->defaultThreshold = $threshold;
+    }
+
+    /**
+     * @param Request $request 
+     * @return array 
+     */
+    public function transform(Request $request): array
     {
         $eloQuery = [];
 
@@ -40,5 +87,66 @@ class LeadQuery
         }
 
         return $eloQuery;
+    }
+
+    /**
+     * @param Request $request 
+     * @return LeadCollection 
+     * @throws InvalidArgumentException 
+     */
+    public function getIndexResults(Request $request): LeadCollection
+    {
+        $queryItems = $this->transform($request);
+        $quality = $request->query('quality');
+        $leads = Lead::where($queryItems)
+            ->whereNull('deleted_at')
+            ->with('address');
+
+        if ($quality) {
+            $billThreshold = env('ELECTRIC_BILL_THRESHOLD', $this->getDefaultThreshold());
+
+            if ($quality === 'premium') {
+                $leads->where('electric_bill', '>=', $billThreshold);
+            } elseif ($quality === 'standard') {
+                $leads->where('electric_bill', '<', $billThreshold);
+            }
+        }
+
+        return new LeadCollection($leads->paginate()->appends($request->query()));
+    }
+
+    /**
+     * @param Request $request 
+     * @return JsonResponse 
+     * @throws BindingResolutionException 
+     */
+    public function createLeadFromApi(Request $request): JsonResponse
+    {
+        $request_params = $request->all();
+        $lead = new LeadResource(Lead::create($request->all()));
+        $request_params['lead_id'] = $lead->id;
+        $address = new AddressResource(Address::create($request_params));
+
+        return response()->json(['lead' => $lead, 'address' => $address]);
+    }
+
+    public function deleteLeadFromApi(Lead $lead)
+    {
+        $lead_id = $lead->id;
+        $code = 200;
+        $message = '';
+
+        try {
+            $lead->delete();
+            $message = sprintf('Lead with ID: %d successfully deleted', $lead_id);
+        } catch (Throwable $e) {
+            $code = 500;
+            $message = $e->getMessage();
+        }
+
+        return response()->json([
+            'code' => $code,
+            'message' => $message
+        ]);
     }
 }
